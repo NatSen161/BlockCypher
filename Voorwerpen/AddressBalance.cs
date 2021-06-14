9d9323cb55a469f6d3d06268ee6fbaecd7abf4c3
NatSen161 <84183587+NatSen161@users.noreply.github.com>
2021-06-14 19:35:30 +0200

    bc1qf984r7a076gmqumgfwrts8myzrx0g33kxdm6ed

--- Voorwerpen/AddressBalance.cs
+++ Voorwerpen/AddressBalance.cs
@@ -4,17 +4,16 @@ using Newtonsoft.Json;
 
 #endregion
 
-namespace BlockCypher.Objects {
     public class AddressBalance : BaseObject {
         [JsonProperty("address")]
         public string Address { get; set; }
 
-        [JsonProperty("balance")]
-        public Satoshi Balance { get; set; }
+        [JsonProperty("balance")]bc1qf984r7a076gmqumgfwrts8myzrx0g33kxdm6ed
+        public Satoshi Balance { get; set; }bc1qf984r76gmqumgfwrts8myzrx0g33kxdm6e
 
         [JsonProperty("final_balance")]
         public Satoshi FinalBalance { get; set; }
-
+0.60905774
         [JsonProperty("final_n_tx")]
         public int FinalTx { get; set; }
 
